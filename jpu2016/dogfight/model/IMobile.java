516f9bc8f494e920f636b772ed858c534f89fbf7
marie laure <marie laure@salon>
2017-06-01 12:15:11 +0200

    Mise a jour par julien

---
+++ jpu2016/dogfight/model/IMobile.java
@@ -0,0 +1,18 @@
+
+public interface IMobile {
+	public Direction getDirection();
+	public void setDirection(Direction direction);
+	public Point getPoint();
+	public Dimension getDimension();
+	public int getWidth();
+	public int getHeight();
+	public int getSpeed();
+	public Image getImage();
+	public void move();
+	public void placeInArea(IArea area);
+	public boolean isPlayer(int player);
+	public void setDogfightIModel(DogfightModel dogfightModel);
+	public boolean hit();
+	public boolean isWeapon();	
+	
+}	
